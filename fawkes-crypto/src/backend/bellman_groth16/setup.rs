421140c5eaaf84e80dd327667a197a2d371ba51c
vladimir popov <rowdyme0@gmail.com>
2022-07-12 20:39:24 +0300

    setup with external params

--- fawkes-crypto/src/backend/bellman_groth16/setup.rs
+++ fawkes-crypto/src/backend/bellman_groth16/setup.rs
@@ -4,8 +4,36 @@ use super::osrng::OsRng;
 use super::*;
 use crate::circuit::cs::BuildCS;
 
-pub fn setup<E: Engine, Pub: Signal<BuildCS<E::Fr>>, Sec: Signal<BuildCS<E::Fr>>, C: Fn(Pub, Sec)>(
+pub fn setup<
+    E: Engine,
+    Pub: Signal<BuildCS<E::Fr>>,
+    Sec: Signal<BuildCS<E::Fr>>,
+    C: Fn(Pub, Sec),
+>(
     circuit: C,
+) -> Parameters<E> {
+    let ref mut rng = OsRng::new();
+
+    let ref rcs = BuildCS::rc_new();
+
+    let bcs = BellmanCS::<E, BuildCS<E::Fr>>::new(rcs.clone());
+
+    let bp: bellman::groth16::Parameters<<E as Engine>::BE> =
+        bellman::groth16::generate_random_parameters(bcs, rng)
+            .unwrap()
+            .to_owned();
+
+    setup_with_params(circuit, bp)
+}
+
+pub fn setup_with_params<
+    E: Engine,
+    Pub: Signal<BuildCS<E::Fr>>,
+    Sec: Signal<BuildCS<E::Fr>>,
+    C: Fn(Pub, Sec),
+>(
+    circuit: C,
+    bp: bellman::groth16::Parameters<<E as Engine>::BE>,
 ) -> Parameters<E> {
     let ref rcs = BuildCS::rc_new();
     let signal_pub = Pub::alloc(rcs, None);
@@ -14,11 +42,8 @@ pub fn setup<E: Engine, Pub: Signal<BuildCS<E::Fr>>, Sec: Signal<BuildCS<E::Fr>>
 
     circuit(signal_pub, signal_sec);
 
-    let bcs = BellmanCS::<E, BuildCS<E::Fr>>::new(rcs.clone());
-
-    let ref mut rng = OsRng::new();
-    let bp = bellman::groth16::generate_random_parameters(bcs, rng).unwrap();
-    let cs=rcs.borrow();
+    // let bp = bellman::groth16::generate_random_parameters(bcs, rng).unwrap();
+    let cs = rcs.borrow();
 
     let num_gates = cs.gates.len();
 
@@ -31,5 +56,10 @@ pub fn setup<E: Engine, Pub: Signal<BuildCS<E::Fr>>, Sec: Signal<BuildCS<E::Fr>>
     c.flush().unwrap();
     drop(c);
 
-    Parameters(bp, num_gates as u32, buf.into_inner(), cs.const_tracker.clone())
+    Parameters(
+        bp,
+        num_gates as u32,
+        buf.into_inner(),
+        cs.const_tracker.clone(),
+    )
 }
