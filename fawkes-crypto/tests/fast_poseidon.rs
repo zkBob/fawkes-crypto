c6095486ad99eb30a1b6eb1098b18fca06475702
Alexander Filippov <aleksander.fill@gmail.com>
2023-02-27 16:06:58 +0300

    Test serialization

--- fawkes-crypto/tests/fast_poseidon.rs
+++ fawkes-crypto/tests/fast_poseidon.rs
@@ -5,16 +5,23 @@ use ff_uint::Num;
 
 #[test]
 fn test_fast_poseidon() {
-    let params: PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_6.json")).unwrap();
+    let params: PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
     let fast_params = FastPoseidonParams::<Fr>::from_original_params(params.clone());
 
     let m = [Num::from(5), Num::from(3)];
     let hash = poseidon(&m, &params);
-
     let fast_hash = fast_poseidon::poseidon::poseidon(&m, &fast_params);
+    assert_eq!(hash, fast_hash);
+}
 
-    println!("{:?}", hash);
-    println!("{:?}", fast_hash);
+#[test]
+fn test_fast_poseidon_params_serialization() {
+    let params: PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
+    let fast_params: FastPoseidonParams<Fr> = serde_json::from_str(include_str!("./res/fast_poseidon_params_t_3.json")).unwrap();
+
+    let m = [Num::from(5), Num::from(3)];
+    let hash = poseidon(&m, &params);
+    let fast_hash = fast_poseidon::poseidon::poseidon(&m, &fast_params);
     assert_eq!(hash, fast_hash);
 }
 
@@ -36,19 +43,4 @@ fn test_fast_poseidon_comp() {
         fast_poseidon::poseidon::poseidon(&m, &fast_params);
     }
     println!("Fast: {:?}", t.elapsed());
-}
-
-
-#[test]
-fn test_fast_poseidon_bench() {
-    let salt = "asdfasdf";
-    let (t, f, p) = (3, 8, 56);
-    let params = PoseidonParams::<Fr>::new_with_salt(t, f, p, salt);
-    let fast_params = FastPoseidonParams::<Fr>::from_original_params(params.clone());
-
-    let m = [Num::from(2), Num::from(3)];
-    let iter_count = 100000;
-    for _ in 0..iter_count {
-        fast_poseidon::poseidon::poseidon(&m, &fast_params);
-    }
 }
