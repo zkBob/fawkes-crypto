e3f3ea5c1427463d1076cf7695391cd95246103a
Alexander Filippov <aleksander.fill@gmail.com>
2023-03-03 14:25:50 +0300

    Rename fast_poseidon

--- fawkes-crypto/tests/optimized_poseidon.rs
+++ fawkes-crypto/tests/optimized_poseidon.rs
@@ -4,36 +4,36 @@ use fawkes_crypto_zkbob::{native::poseidon::{unoptimized, optimized}, engines::b
 use ff_uint::Num;
 
 #[test]
-fn test_fast_poseidon() {
+fn test_optimized_poseidon() {
     let params: unoptimized::params::PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
-    let fast_params = optimized::params::PoseidonParams::<Fr>::from_original_params(params.clone());
+    let optimized_params = optimized::params::PoseidonParams::<Fr>::from_original_params(params.clone());
 
     let m = [Num::from(5), Num::from(3)];
     let hash = unoptimized::poseidon::poseidon(&m, &params);
-    let fast_hash = optimized::poseidon::poseidon(&m, &fast_params);
-    assert_eq!(hash, fast_hash);
+    let optimized_hash = optimized::poseidon::poseidon(&m, &optimized_params);
+    assert_eq!(hash, optimized_hash);
 }
 
 #[test]
-fn test_fast_poseidon_params_serialization() {
+fn test_optimized_poseidon_params_serialization() {
     let t = Instant::now();
     let params: unoptimized::params::PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
-    println!("Original: {:?}", t.elapsed());
+    println!("Unoptimized: {:?}", t.elapsed());
 
     let t = Instant::now();
-    let fast_params: optimized::params::PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
-    println!("Fast: {:?}", t.elapsed());
+    let optimized_params: optimized::params::PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
+    println!("Optimized: {:?}", t.elapsed());
 
     let m = [Num::from(5), Num::from(3)];
     let hash = unoptimized::poseidon::poseidon(&m, &params);
-    let fast_hash = optimized::poseidon::poseidon(&m, &fast_params);
-    assert_eq!(hash, fast_hash);
+    let optimized_hash = optimized::poseidon::poseidon(&m, &optimized_params);
+    assert_eq!(hash, optimized_hash);
 }
 
 #[test]
-fn test_fast_poseidon_comp() {
+fn test_optimized_poseidon_comp() {
     let params: unoptimized::params::PoseidonParams<Fr> = serde_json::from_str(include_str!("./res/poseidon_params_t_3.json")).unwrap();
-    let fast_params = optimized::params::PoseidonParams::<Fr>::from_original_params(params.clone());
+    let optimized_params = optimized::params::PoseidonParams::<Fr>::from_original_params(params.clone());
 
     let m = [Num::from(2), Num::from(3)];
     let iter_count = 10000;
@@ -41,11 +41,11 @@ fn test_fast_poseidon_comp() {
     for _ in 0..iter_count {
         unoptimized::poseidon::poseidon(&m, &params);
     }
-    println!("Original: {:?}", t.elapsed());
+    println!("Unoptimized: {:?}", t.elapsed());
 
     let t = Instant::now();
     for _ in 0..iter_count {
-        optimized::poseidon::poseidon(&m, &fast_params);
+        optimized::poseidon::poseidon(&m, &optimized_params);
     }
-    println!("Fast: {:?}", t.elapsed());
+    println!("Optimized: {:?}", t.elapsed());
 }
